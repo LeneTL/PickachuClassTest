84b343dc7e01f8008e739e2f56f467c9019e2cd4
Lene <Lene-tl@hotmail.com>
2025-12-03 14:44:33 +0100

    init

---
+++ PicachuKlasse/Pokemon.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace PicachuKlasse
+{
+    public class Pokemon
+    {
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public int Level { get; set; }
+    }
+    
+}
---
+++ PicachuKlasse/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace PicachuKlasse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var Pikachu = new Pokemon { Name= "Pikachu", Health = 50, Level = 10};
+            
+            Console.WriteLine($"{Pikachu.Name} is Level {Pikachu.Level} and has {Pikachu.Health}Hp");
+        }
+    }
+}
